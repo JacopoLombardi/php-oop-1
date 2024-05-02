467e6d92f8a6c16e1879d8ea37d35c43a1d716ce
JacopoLombardi <lombajacopo@gmail.com>
2024-05-02 15:20:54 +0200

    [add] added 'variabili d'istanza' & 'construct'

--- Model/Movie.php
+++ Model/Movie.php
@@ -4,9 +4,20 @@
 class Movie{
    public $title;
    public $description;
+   public $type;
+   public $year;
+
+   // construct
+   public function __construct(string $_title, string $_description, string $_type, int $_year){
+      $this->title = $_title;
+      $this->description = $_description;
+      $this->type = $_type;
+      $this->year = $_year;
+   }
+
+
+
 }
-
-
 
 
 ?>
